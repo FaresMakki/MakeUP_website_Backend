0878dab58c63f54f6d6989996615e6431848353c
benbelgacem azmi <benbelgacemazmi@gmail.com>
2024-07-09 11:04:31 +0100

    last change on branch

--- Controller/Discount.js
+++ Controller/Discount.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Discount = require('../models/Discount');
 exports.createDiscount = async (req, res) => {
 
@@ -60,31 +59,3 @@ exports.deleteDiscount = async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 };
-=======
-const discount = require("../Models/Discount");
-exports.adddiscount=async (req,res)=>{
-    try {
-
-        
-        console.log("hi im here")
-        console.log(req.body)
-        const model = {
-            Product_id: req.body.azmi,
-            discountper:req.body.fares,
-            startdate:req.body.startdate,
-            enddate:req.body.dateend
-        };
-
-        console.log("imhere")
-        const dis =new discount(model);
-        console.log("voooo")
-        const saveddis = await dis.save();
-        res.status(200).json({ saveddis });
-    } catch (err) {
-        res.status(400).json({ err });
-    }
-
-
-
-}
->>>>>>> d0e0cc3442ae80ef503858f37fc67e5a061e8c31
--- Models/Discount.js
+++ Models/Discount.js
@@ -1,6 +1,5 @@
 const mongoose=require("mongoose")
 const discountSchema=new mongoose.Schema({
-<<<<<<< HEAD
     Discount_id:{
         type:String,
         required:true,
@@ -18,29 +17,11 @@ const discountSchema=new mongoose.Schema({
         required:true
     },
     End_date:{
-=======
-    Product_id:{
-        type:String,
-        required:true,
-    },
-    discountper:{
-        type:Number,
-        required:true
-    },
-    startdate:{
-        type:String,
-        required:true
-    },
-    enddate:{
->>>>>>> d0e0cc3442ae80ef503858f37fc67e5a061e8c31
         type:String,
         required:true
     },
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> d0e0cc3442ae80ef503858f37fc67e5a061e8c31
 },{timestamps:true})
 module.exports=mongoose.model('discount',discountSchema)
